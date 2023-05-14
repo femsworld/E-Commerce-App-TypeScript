d8e08ba34c2a8a09a76f4514094b9b8310815b1a
femsworld <femsworld@gmail.com>
2023-05-14 03:41:06 +0300

    Created the global store

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const App = () => {
+  const globalState = useSelector(state => state)
+  console.log("globalSate: ", globalState)
   return (
-    <div>App</div>
+      <div>
+          App
+      </div>
   )
 }
 
