c48a6d3e9598cbb09307472b3e72ff524923d3c0
femsworld <femsworld@gmail.com>
2023-05-15 14:12:34 +0300

    Fetched all users

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
 import { GlobalState } from './redux/store'
 import useAppSelector from './hooks/useAppSelector'
+import { createUser, fetchAllUsers, updateUserReducer } from './redux/reducers/usersReducer'
+import useAppDispatch from './hooks/useAppDispatch'
+import axios from 'axios'
 
 const App = () => {
-  // const globalState = useSelector(state => state)
-  // console.log("globalSate: ", globalState)
-  // const users = useSelector((state: GlobalState) => state.usersReducer)
-  const users = useAppSelector(state => state.usersReducer )
-  console.log("users: ", users)
+  const users = useAppSelector(state => state.usersReducer)
+  console.log(users)
+  const dispatch = useAppDispatch()
+  const addUser = () => {
+    dispatch(createUser(
+      {
+        id: 2,
+        email: "",
+        password: "",
+        role: "admin",
+        name: "Tom",
+        avatar: "string",
+    }))
+    // console.log("This is a new user: ", createUser({}))
+  }
+
+  useEffect(() => {
+    dispatch(fetchAllUsers())
+  }, [])
+
   return (
       <div>
-          App
+          <button onClick={addUser}>Create new user</button>
       </div>
   )
 }
--- src/redux/reducers/usersReducer.ts
+++ src/redux/reducers/usersReducer.ts
@@ -1,5 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { User } from "../../types/User";
+import axios, { AxiosError } from "axios";
 
 const initialState: User[] = [
     {
@@ -12,21 +13,49 @@ const initialState: User[] = [
     }
 ]
 
+ export const fetchAllUsers = createAsyncThunk(
+    "fetchAllUsers",
+    async () => {
+        try {
+            const result = await axios.get<User[]>("https://api.escuelajs.co/api/v1/users")
+            return result.data
+        } catch (e) {
+            const error = e as AxiosError
+            if(error.request) {
+                console.log("error in request: ", error.request)
+            } else {
+                console.log(error.response?.data)
+            }
+        }
+    }
+    
+)
+
 const usersSlice = createSlice({
     name: "users",
     initialState,
     reducers: {
-        createUser: (state, action) => {
+        createUser: (state, action: PayloadAction<User>) => {
             state.push(action.payload)
+        },
+        updateUserReducer: (state, action: PayloadAction<User[]>) => {
+            return action.payload
         }
+    },
+    extraReducers: (build) => {
+        build.addCase(fetchAllUsers.fulfilled, (state, action) => {
+            if(action.payload) {
+                return action.payload
+            }
+        })
     }
-    
 })
 
 const usersReducer = usersSlice.reducer
 export const
     {
         createUser,
+        updateUserReducer
         
     } = usersSlice.actions
-export default usersReducer
+export default usersReducer 
